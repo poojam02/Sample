b7db31b9ce3090bd92202845352b144d96b2b270
poojam02 <60595042+poojam02@users.noreply.github.com>
2021-07-24 22:23:52 +0530

    Add files via upload

---
+++ stack.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int arr[10],n,i;
+    cout<<"\nEnter the number of elements: ";
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"\nElements in the array.."<<arr[i];
+}
